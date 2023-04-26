c04c3736fd17d984b8b1c100d58a2f1f1fa483c9
Alf Eaton <alf.eaton@overleaf.com>
2023-04-26 13:17:54 +0100

    Export togglePanel and createSearchPanel

--- src/search.ts
+++ src/search.ts
@@ -337,7 +337,7 @@ class RegExpQuery extends QueryType<RegExpResult> {
 /// once).
 export const setSearchQuery = StateEffect.define<SearchQuery>()
 
-const togglePanel = StateEffect.define<boolean>()
+export const togglePanel = StateEffect.define<boolean>()
 
 const searchState: StateField<SearchState> = StateField.define<SearchState>({
   create(state) {
@@ -521,7 +521,7 @@ export const replaceAll = searchCommand((view, {query}) => {
   return true
 })
 
-function createSearchPanel(view: EditorView) {
+export function createSearchPanel(view: EditorView) {
   return view.state.facet(searchConfigFacet).createPanel(view)
 }
 
